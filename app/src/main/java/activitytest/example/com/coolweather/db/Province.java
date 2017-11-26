a877908bb96c247ac67ccca9d4e3eae976277033
33324 <33324203@qq.com>
2017-11-26 22:09:31 +0800

    first commit

--- app/src/main/java/activitytest/example/com/coolweather/db/Province.java
+++ app/src/main/java/activitytest/example/com/coolweather/db/Province.java
@@ -1,8 +1,33 @@
 package activitytest.example.com.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by 33324 on 2017/11/26.
  */
 
-public class Province {
+public class Province extends DataSupport {
+    private  int id;
+    private  String provinceName;
+    private  int provinceCode;
+    public  int getId(){
+        return id;
+    }
+    public  void  setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+
+
+    }
 }
